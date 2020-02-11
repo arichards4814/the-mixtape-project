e925415ef6e801bcc0b8c87c03a365ae1c759bc9
arichards4814 <arichards4814@gmail.com>
2020-02-11 09:09:53 -0500

    added javascript

--- app/javascript/custom.js
+++ app/javascript/custom.js
@@ -6,3 +6,20 @@ window.updateText = function () {
     document.getElementById('message-text').innerHTML = document.getElementById('title').value
 
 }
+
+window.updateCassette = function (e) {
+
+    console.log(e)
+    document.getElementById('cassette').src = e.src
+    document.getElementById('mixtape_cassette_url').value = e.name
+
+}
+
+window.updateBackground = function (e) {
+
+    console.log(e)
+    document.getElementById('background').src = e.src
+    document.getElementById('mixtape_background_url').value = e.name
+
+}
+
