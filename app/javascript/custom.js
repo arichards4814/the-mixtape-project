08ba84ded5ebdc9c80a7fb29a9a06039074e7dd2
arichards4814 <arichards4814@gmail.com>
2020-02-10 20:56:59 -0500

    added some javascript

---
+++ app/javascript/custom.js
@@ -0,0 +1,8 @@
+window.testFun = function () {
+    alert('Hello World!');
+}
+
+window.updateText = function () {
+    document.getElementById('message-text').innerHTML = document.getElementById('title').value
+
+}
