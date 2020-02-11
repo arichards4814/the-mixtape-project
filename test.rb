89412b8c8755191ab6d28d84a9b597a8bb43ca8c
arichards4814 <arichards4814@gmail.com>
2020-02-11 17:32:56 -0500

    removed sc

--- test.rb
+++
@@ -1,9 +0,0 @@
-require 'rspotify'
-
-RSpotify.authenticate("45935d113e3d4f9a9e8c59ac455ccd0e","57d0101195234b9e84b4e9946d5af3ba")
-
-artists = RSpotify::Artist.search('Arctic Monkeys')
-
-test = artists.first
-
-p test.albums.first.tracks.first.href
